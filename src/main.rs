18fffad43aeba8ff387371cfd39cfe7385050163
igorsheg <igorsheg@gmail.com>
2024-08-04 16:11:23 +0300

    ci(web): switch to pnpm

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
+use crate::config::Configuration;
 use application::{
     client::service::ClientService, game::service::GameService, room::service::RoomService,
 };
 use domain::events::Topic;
 use infra::{error::Error, event_emmiter::EventEmitter, server::Server};
-use tracing_loki::url::Url;
 use tracing_subscriber::{prelude::*, EnvFilter};
-
-use crate::config::Configuration;
 
 pub mod application;
 pub mod config;
@@ -14,18 +12,23 @@ pub mod domain;
 pub mod infra;
 pub mod presentation;
 
+#[cfg(feature = "loki")]
+use tracing_loki::url::Url;
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let config = Configuration::new();
-
     let filter = EnvFilter::from_default_env();
 
+    #[cfg(feature = "loki")]
     let loki_url = std::env::var("LOKI_URL")?;
 
+    #[cfg(feature = "loki")]
     let (loki_tracing_layer, loki_tracing_task) = tracing_loki::builder()
         .label("host", "set-up-rust")?
         .build_url(Url::parse(&loki_url).unwrap())?;
 
+    #[cfg(feature = "loki")]
     let subscriber = tracing_subscriber::fmt()
         .with_env_filter(filter)
         .with_ansi(true)
@@ -34,6 +37,15 @@ async fn main() -> Result<(), Error> {
         .compact()
         .finish()
         .with(loki_tracing_layer);
+
+    #[cfg(not(feature = "loki"))]
+    let subscriber = tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_ansi(true)
+        .pretty()
+        .json()
+        .compact()
+        .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("Setting global default failed");
 
@@ -61,9 +73,10 @@ async fn main() -> Result<(), Error> {
         game_controller.clone(),
     );
 
+    #[cfg(feature = "loki")]
     tokio::spawn(loki_tracing_task);
 
     server.run().await;
-
     Ok(())
 }
+
